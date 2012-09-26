244186faac33d3eb8b5ba9f7ad49fa14428c9853
terase <tokyokansai@gmail.com>
2012-09-26 17:57:57 +0900

    background.html削除 chrome Ver20以降使えなくなるようにした javascriptのコーディングの構造変更 extensionIdを固定値廃止 scrollイベントハンドラの追加（多分、ボタン表示しないバグは少なくなる） ほか多数

--- src/content_scripts/script.js
+++ src/content_scripts/script.js
@@ -1,38 +1,73 @@
 jQuery.noConflict();
-(function($, extention_id) {
-    var $win = $(window);
-    $win.bottom();
-    var base_path = 'chrome-extension://'+extention_id+'/';
-	$().ready(function() {
+jQuery.holdReady(true);
+/**
+ * Factory
+ */
+function Factory(o) {
+  var f = Factory.f, i, len, n, prop;
+  f.prototype = o;
+  n = new f;
+  for (i=1, len=arguments.length; i<len; ++i)
+    for (prop in arguments[i])
+      n[prop] = arguments[i][prop];
+  return n;
+}
+Factory.f = function(){};
 
-        var $img = $('<img/>').attr('id','_creepup_ir77_').attr('src', base_path+'icon/64.png');
-		if(!$img)
-		    return;
-        
-        var hasScroll = $(document).height() > $win.height();
-        
-		$('body').append($img);
+/**
+ * Contents
+ */
+var sUpperProto = {
 
-        var x = $win.scrollLeft() + $win.width() - $img.width() - "80",
-            y = $win.scrollTop() + $win.height() - $img.height() - "80";
-        $img.css({position : 'fixed', left : x + 'px', top : y + 'px'});
+    extention_id : chrome.runtime.id,
 
-        hasScroll ? $img.show() : $img.hide();
-        $img.on('click', function() {
-            $("html,body").animate({ scrollTop : 0 }, 'fast');
-            hasScroll ? $(this).show() : $(this).hide();
+    put : function($) {
+	
+	    var $win = $(window);
+	    $win.bottom();
+	    var base_path = 'chrome-extension://'+this.extention_id+'/';
+
+        $().ready(function() {
+
+	        var $img = $('<img/>').attr('id','_creepup_ir77_').attr('src', base_path+'icon/64.png');
+	        if(!$img)
+	            return;
+	        
+	        var hasScroll = $(document).height() > $win.height();
+	        console.log("hasScroll:"+hasScroll);
+	        $('body').append($img);
+
+	        var x = $win.scrollLeft() + $win.width() - $img.width() - "80",
+	            y = $win.scrollTop() + $win.height() - $img.height() - "80";
+	        $img.css({position : 'fixed', left : x + 'px', top : y + 'px'});
+
+	        hasScroll ? $img.show() : $img.hide();
+	        $img.on('click', function() {
+	            $("html,body").animate({ scrollTop : 0 }, 'fast');
+	            hasScroll ? $(this).show() : $(this).hide();
+	        });
+
+            $win.on("bottom", function () {
+	            var $handler = $(this);
+	            if(!$handler.data('loading')) {
+                    $handler.data('loading', true);
+	                hasScroll ? $img.show() : $img.hide();
+                    $handler.data('loading', false);
+	            }
+            });
+
+            $win.on("scroll", function () {
+	            hasScroll = $(document).height() > $win.height();
+                hasScroll ? $img.show() : $img.hide();
+            });
         });
 
-	    $win.on("bottom", function () {
-		    var $handler = $(this);
-		    if(!$handler.data('loading')) {
-			    $handler.data('loading', true);
-                hasScroll ? $img.show() : $img.hide();
-                    
-			    $handler.data('loading', false);
+    }
+	
+};
 
-		    }
-	    });
-	});
+// Main
+var sUpper = Factory(sUpperProto);
+sUpper.put(jQuery);
+jQuery.holdReady(false);
 
-})(jQuery, 'adodpoahopofoedfbkbmmomebddldnbo'); // param1:jQueryObject, param2:ChromeExtentionId
